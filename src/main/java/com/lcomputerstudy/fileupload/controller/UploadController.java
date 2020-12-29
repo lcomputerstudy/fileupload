8298efc341a6446752c6da0dc4cae2b80d0e1739
lcomputerstudy <lcomputerstudy@naver.com>
2020-12-29 14:34:08 +0900

    thumbnail

--- src/main/java/com/lcomputerstudy/fileupload/controller/UploadController.java
+++ src/main/java/com/lcomputerstudy/fileupload/controller/UploadController.java
@@ -1,8 +1,13 @@
 package com.lcomputerstudy.fileupload.controller;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,12 +34,34 @@ public class UploadController {
 		return "/upload";
 	}
 	
-	@RequestMapping(value="upload", method=RequestMethod.POST)
+	// 업로드
+	@RequestMapping(value="/upload", method=RequestMethod.POST)
 	public String upload(@RequestParam("file") MultipartFile multipartFile) {
-		File file = new File("/Users/lcomputer/Documents/workspace/fileupload/src/main/resources/static/images/" + multipartFile.getOriginalFilename());
+		String path = "/Users/lcomputer/Documents/work/fileupload/src/main/resources/static/images/";
+		String thumbPath = path + "thumb/";
+		String filename = multipartFile.getOriginalFilename();
+		String ext = filename.substring(filename.lastIndexOf(".")+1);
+		
+		File file = new File(path + filename);
+		File thumbFile = new File(thumbPath + filename);
 		try {
+			// 원본파일 저장
 			InputStream input = multipartFile.getInputStream();
 			FileUtils.copyInputStreamToFile(input, file);
+			
+			// 썸네일 생성
+			BufferedImage imageBuf = ImageIO.read(file);
+			int fixWidth = 500;
+			double ratio = imageBuf.getWidth() / (double)fixWidth;
+			int thumbWidth = fixWidth;
+			int thumbHeight = (int)(imageBuf.getHeight() / ratio);
+			BufferedImage thumbImageBf = new BufferedImage(thumbWidth, thumbHeight, BufferedImage.TYPE_3BYTE_BGR);
+			Graphics2D g = thumbImageBf.createGraphics();
+			Image thumbImage = imageBuf.getScaledInstance(thumbWidth, thumbHeight, Image.SCALE_SMOOTH);
+			g.drawImage(thumbImage, 0, 0, thumbWidth, thumbHeight, null);
+			g.dispose();
+			ImageIO.write(thumbImageBf, ext, thumbFile);
+			
 		} catch (IOException e) {
 			FileUtils.deleteQuietly(file);
 			e.printStackTrace();
